69eea92276890abdcb6b47abe2700d3de0c8d984
barioto <pedro.barioto@luizalabs.com>
2018-12-14 16:39:40 -0200

    fix setup py

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ import re
 import os
 
 from setuptools import setup, find_packages
-from distutils.util import strtobool
 
 with open(
         os.path.join(
@@ -10,41 +9,36 @@ with open(
             'aiocache/_version.py')) as fp:
     try:
         version = re.findall(
-            r"^__version__ = '([^']+)'\r?$", fp.read(), re.M)[0]
+            r"^__version__ = \"([^']+)\"\r?$", fp.read(), re.M)[0]
     except IndexError:
         raise RuntimeError('Unable to determine version.')
 
 
-aioredis = "aioredis>=0.3.3,<1"
-aiomcache = "aiomcache>=0.5.2"
+with open('README.rst', 'rt', encoding='utf8') as f:
+    readme = f.read()
 
-install_requires = set((aioredis, aiomcache))
 
-setup_kwargs = {
-    'name': "aiocache",
-    'version': version,
-    'author': "Manuel Miranda",
-    'url': "https://github.com/argaen/aiocache",
-    'author_email': "manu.mirandad@gmail.com",
-    'description': "multi backend asyncio cache",
-    'classifiers': [
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Framework :: AsyncIO",
+setup(
+    name='aiocache',
+    version=version,
+    author='Manuel Miranda',
+    url='https://github.com/argaen/aiocache',
+    author_email='manu.mirandad@gmail.com',
+    description='multi backend asyncio cache',
+    long_description=readme,
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Framework :: AsyncIO',
     ],
-    'packages': find_packages(),
-}
-
-
-if not strtobool(os.environ.get("AIOCACHE_REDIS", "yes")):
-    print("Installing without aioredis")
-    install_requires.remove(aioredis)
-
-if not strtobool(os.environ.get("AIOCACHE_MEMCACHED", "yes")):
-    print("Installing without aiomcache")
-    install_requires.remove(aiomcache)
-
-
-setup_kwargs['install_requires'] = install_requires
-setup(**setup_kwargs)
+    packages=find_packages(),
+    install_requires=[],
+    extras_require={
+        'redis:python_version<"3.7"': ['aioredis>=0.3.3'],
+        'redis:python_version>="3.7"': ['aioredis>=1.0.0'],
+        'memcached': ['aiomcache>=0.5.2'],
+        'msgpack': ['msgpack'],
+    }
+)
